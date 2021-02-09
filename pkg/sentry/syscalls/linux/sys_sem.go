b8ae99af4053e9e98bc04e41984f3caf72a8619b
Jing Chen <chjing@google.com>
2021-02-09 02:23:31 -0800

    Implement SEM_STAT_ANY cmd of semctl.
    PiperOrigin-RevId: 356456822

--- pkg/sentry/syscalls/linux/sys_sem.go
+++ pkg/sentry/syscalls/linux/sys_sem.go
@@ -220,8 +220,16 @@ func Semctl(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.Syscal
 		return uintptr(semid), nil, err
 
 	case linux.SEM_STAT_ANY:
-		t.Kernel().EmitUnimplementedEvent(t)
-		fallthrough
+		arg := args[3].Pointer()
+		// id is an index in SEM_STAT.
+		semid, ds, err := semStatAny(t, id)
+		if err != nil {
+			return 0, nil, err
+		}
+		if _, err := ds.CopyOut(t, arg); err != nil {
+			return 0, nil, err
+		}
+		return uintptr(semid), nil, err
 
 	default:
 		return 0, nil, syserror.EINVAL
@@ -272,6 +280,22 @@ func semStat(t *kernel.Task, index int32) (int32, *linux.SemidDS, error) {
 	}
 	creds := auth.CredentialsFromContext(t)
 	ds, err := set.GetStat(creds)
+	if err != nil {
+		return 0, ds, err
+	}
+	return set.ID, ds, err
+}
+
+func semStatAny(t *kernel.Task, index int32) (int32, *linux.SemidDS, error) {
+	set := t.IPCNamespace().SemaphoreRegistry().FindByIndex(index)
+	if set == nil {
+		return 0, nil, syserror.EINVAL
+	}
+	creds := auth.CredentialsFromContext(t)
+	ds, err := set.GetStatAny(creds)
+	if err != nil {
+		return 0, ds, err
+	}
 	return set.ID, ds, err
 }
 
